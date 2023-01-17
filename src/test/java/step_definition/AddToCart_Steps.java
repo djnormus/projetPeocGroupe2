59532bab578f15cbf69e1cef4d9e6b4b324a563f
Nordine <nordine31@gmail.com>
2023-01-17 16:23:10 +0100

    Add to cart feature -completed

--- src/test/java/step_definition/AddToCart_Steps.java
+++ src/test/java/step_definition/AddToCart_Steps.java
@@ -1,16 +1,23 @@
 package step_definition;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.BasketPageLocators;
 import pages.ShopPageLocators;
+
+import java.time.Duration;
 
 import static step_definition.Hooks.driver;
 
 public class AddToCart_Steps {
 
     ShopPageLocators shopLoc = new ShopPageLocators(driver);
+    BasketPageLocators basketLocator = new BasketPageLocators(driver);
 
     @Then("chaque article contient une photo descriptive, un libellé et le prix affiché au-dessous")
     public void chaqueArticleContientUnePhotoDescriptiveUnLibelléEtLePrixAffichéAuDessous() {
@@ -20,9 +27,67 @@ public class AddToCart_Steps {
             Assert.assertTrue("La photo n'est pas affichée", article.findElement(By.className("wp-post-image")).isDisplayed());
             Assert.assertTrue("Le titre n'est pas affiché",article.findElement(By.tagName("h3")).isDisplayed());
             Assert.assertTrue("Le prix n'est pas affichée",article.findElement(By.className("price")).isDisplayed());
-
         }
 
+
+    }
+
+    @Then("Le lien READ MORE est affiché sur les articles indisponibles")
+    public void leLienREADMOREEstAffichéSurLesArticlesIndisponibles() {
+
+        Assert.assertTrue("Le bouton READ MORE n'est pas affiché", shopLoc.readMoreBtn.isDisplayed());
+    }
+
+    @And("Je clique sur ADD TO BASKET sous un article")
+    public void jeCliqueSurADDTOBASKETSousUnArticle() {
+
+        shopLoc.addToBasketBtn.click();
+    }
+
+
+    @Then("Un lien VIEW BASKET apparait en dessous de l article")
+    public void unLienVIEWBASKETApparaitEnDessousDeLArticle() {
+
+        // EXPLICIT WAIT
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='View Basket']")));
+
+        Assert.assertTrue("Le lien VIEW BASKET n'apparait pas", shopLoc.viewBasketLink.isDisplayed());
+    }
+
+    @And("je clique sur le bouton view basket")
+    public void jeCliqueSurLeBoutonViewBasket() {
+
+        shopLoc.viewBasketLink.click();
+    }
+
+    @Then("la page panier s affiche correctement avec l article ajouté")
+    public void laPagePanierSAfficheCorrectementAvecLArticleAjouté() {
+
+        Assert.assertTrue("Aucun article n'est dans le panier", basketLocator.cartItem.isDisplayed());
+
+    }
+
+    @Then("Un filtre permet d'affiner la vue des articles par prix est présent")
+    public void unFiltrePermetDAffinerLaVueDesArticlesParPrixEstPrésent() {
+
+        Assert.assertTrue("Le filtre par prix n'est pas affiché", shopLoc.filterByPrice.isDisplayed());
+    }
+
+    @And("Un filtre permet d'affiner la vue des articles par theme est présent")
+    public void unFiltrePermetDAffinerLaVueDesArticlesParThemeEstPrésent() {
+
+        Assert.assertTrue("Le filtre par prix n'est pas affiché", shopLoc.filterByTheme.isDisplayed());
+    }
+
+    @Then("La page Shop contient les articles en vente")
+    public void laPageShopContientLesArticlesEnVente() {
+
+        //Assert.assertTrue("La photo n'est pas affichée", article.findElement(By.className("wp-post-image")).isDisplayed());
+        //Assert.assertTrue("Le titre n'est pas affiché",article.findElement(By.tagName("h3")).isDisplayed());
+        //Assert.assertTrue("Le prix n'est pas affichée",article.findElement(By.className("price")).isDisplayed());
+
+        Assert.assertTrue("Le filtre par prix n'est pas affiché", shopLoc.oneItem.isDisplayed());
 
     }
 }
--- src/test/java/step_definition/Hooks.java
+++ src/test/java/step_definition/Hooks.java
@@ -62,7 +62,7 @@ public class Hooks {
 
     @After
     public void AfterTest() {
-       // driver.quit();
+       driver.quit();
     }
 
 
